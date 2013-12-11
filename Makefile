bdd5efba3215cd9a71cb71dee430b569e646db98
Donald Ness <donald.ness@gmail.com>
2013-12-10 18:21:57 -0700

    Hacked to work for MOSS instead

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-SERIALPORT=/dev/cu.Cubelet-RGB-AMP-SPP# Paired Bluetooth Cubelet
-CUBELETS=40834 30241# List of Cubelet IDs to control
+SERIALPORT=/dev/cu.Moss-PWG-AMP-SPP# Paired Moss Brain
+FACES=1 3# List of Moss Brain faces to control
 
 leap:
-	node leap $(SERIALPORT) $(CUBELETS)
+	node leap $(SERIALPORT) $(FACES)
 
 .PHONY: leap
--- leap.js
+++ leap.js
@@ -1,23 +1,21 @@
 if (process.argv.length < 3) {
-  console.log('Usage: node run.js PORT [ID1] [ID2]');
+  console.log('Usage: node leap.js PORT [ID1] [ID2]');
   return;
 }
 
 var device = process.argv[2];
 var SerialPort = require('serialport').SerialPort;
-var bluetoothCubelet = new SerialPort(device, { baudrate: 38400 });
-
-var cubelets = require('cubelets');
-var cubelet1ID = process.argv.length >= 4 ? process.argv[3] : 0;
-var cubelet2ID = process.argv.length >= 5 ? process.argv[4] : 0;
-var cubelet1Value = 0;
-var cubelet2Value = 0;
+var mossBrain = new SerialPort(device, { baudrate: 115200 });
+var mossBrainFace1Index = process.argv.length >= 4 ? parseInt(process.argv[3]) : -1;
+var mossBrainFace2Index = process.argv.length >= 5 ? parseInt(process.argv[4]) : -1;
+var mossBrainFace1Value = 0;
+var mossBrainFace2Value = 0;
 
 var Leap = require('leapjs');
 var controller = new Leap.Controller();
 var interactionBox = null;
 
-function mapToCubeletValue(t) {
+function mapToMossBrainFaceValue(t) {
   var p = interactionBox.normalizePoint(t);
   var x = p[0];
   var y = p[1];
@@ -25,45 +23,45 @@ function mapToCubeletValue(t) {
   return Math.max(0, Math.min(Math.round(255 * y), 255));
 }
 
-bluetoothCubelet.on('open', function() {
-  console.log('Bluetooth Cubelet connected...');
+mossBrain.on('open', function() {
+  console.log('Moss Brain connected...');
+  mossBrain.write(new Buffer([
+    '<'.charCodeAt(0),
+    'c'.charCodeAt(0), 0, 8,
+    '>'.charCodeAt(0), 0, 0, 0, 0, 0, 0, 0, 0
+  ]));
   setInterval(function() {
-    console.log(cubelet1Value, cubelet2Value);
-    if (cubelet1ID) {
-      bluetoothCubelet.write(new cubelets
-        .SetBlockValueCommand(cubelet1ID, cubelet1Value)
-          .encode());
+    console.log(mossBrainFace1Value, mossBrainFace2Value);
+    if (mossBrainFace1Index > 0) {
+      mossBrain.write(new Buffer([
+        '<'.charCodeAt(0), 'v'.charCodeAt(0), 0, 2, '>'.charCodeAt(0), mossBrainFace1Index, mossBrainFace1Value
+      ]));
     }
-    if (cubelet2ID) {
-      bluetoothCubelet.write(new cubelets
-        .SetBlockValueCommand(cubelet2ID, cubelet2Value)
-          .encode());
+    if (mossBrainFace2Index > 0) {
+      mossBrain.write(new Buffer([
+        '<'.charCodeAt(0), 'v'.charCodeAt(0), 0, 2, '>'.charCodeAt(0), mossBrainFace2Index, mossBrainFace2Value
+      ]));
     }
-  }, 250);
+  }, 50);
 });
 
-var lastFrame = null;
 controller.on("frame", function(frame) {
   if (frame.valid) {
-    if (!lastFrame) {
-      lastFrame = frame;
-    }
     if (!interactionBox) {
       interactionBox = new Leap.InteractionBox(frame.interactionBox);
     }
     if (frame.hands.length >= 1) {
-      cubelet1Value = mapToCubeletValue(frame.hands[0].palmPosition);
+      mossBrainFace1Value = mapToMossBrainFaceValue(frame.hands[0].palmPosition);
     }
     else {
-      cubelet1Value = 0;
+      mossBrainFace1Value = 127;
     }
     if (frame.hands.length >= 2) {
-      cubelet2Value = mapToCubeletValue(frame.hands[1].palmPosition);
+      mossBrainFace2Value = mapToMossBrainFaceValue(frame.hands[1].palmPosition);
     }
     else {
-      cubelet2Value = 0;
+      mossBrainFace2Value = 127;
     }
-    lastFrame = frame;
   }
 });
 
